64f3327ec9585c925473d0b078d17cb7fc10772a
myname21-meet <myname21@meet.mit.edu>
2019-11-07 12:02:28 -0500

    dfd

--- app.py
+++ app.py
@@ -4,13 +4,19 @@ from flask import session as login_session
 app = Flask(__name__)
 app.secret_key = "MY_SUPER_SECRET_KEY"
 
+@app.route('/')
+def hello_world():
+    return render_template("home.html")
 
-##### Code here ######
+@app.route('/store')
+def store_rout():
+    return render_template("store.html")
 
-
-
-#####################
-
-
+@app.route('/cart')
+def cart_rout():
+    return render_template("cart.html")
+@app.route('/about')
+def about_rout():
+	return render_template("about.html")
 if __name__ == '__main__':
     app.run(debug=True)
